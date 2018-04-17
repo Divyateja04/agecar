18d721c476671f86c8e8ac157b7fba2e172ddb67
Divyateja Pasupuleti <divyateja2004@gmail.com>
2018-04-17 13:01:16 +0530

    Updated

---
+++ Assets/Javascript/1.js
@@ -0,0 +1,12 @@
+function checkDriverAge(){
+		var age = prompt("Your Age ??");
+	if (Number(age) < 18){
+		alert("Sorry, you are too young to drive this car. Powering off")
+	}else if(Number(age) > 18){
+		alert("Powering On. Enjoy the ride!")
+	}else if(Number(age) === " "){
+		alert("Invalid Entry")
+	}else{
+		alert("Congratulations on your first year of driving. Enjoy the ride!")
+	}
+}
