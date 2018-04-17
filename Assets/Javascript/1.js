9ce0f5dd959e3aff602a0a9d4b7054f8c4cd9470
Divyateja Pasupuleti <divyateja2004@gmail.com>
2018-04-17 18:21:11 +0530

    udated bg

--- Assets/Javascript/1.js
+++ Assets/Javascript/1.js
@@ -4,9 +4,10 @@ function checkDriverAge(){
 		alert("Sorry, you are too young to drive this car. Powering off")
 	}else if(Number(age) > 18){
 		alert("Powering On. Enjoy the ride!")
-	}else if(Number(age) === " "){
+	}else if(Number(age) === null){
 		alert("Invalid Entry")
 	}else{
 		alert("Congratulations on your first year of driving. Enjoy the ride!")
+		document.body.style.backgroundImage = "url('../Images/welcome2.jpg')";
 	}
 }
